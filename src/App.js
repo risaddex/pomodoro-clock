aca8c27ef6d5eb1be29ffee6b8cdafcca0fd851f
risaddex <risaddex@gmail.com>
2021-02-04 15:19:14 -0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+const App = () => {
+  return(
+    <>
+       Hero Word
+    </>
+  );
+}
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('app')
+);
+
+export default App;
